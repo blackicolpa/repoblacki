2f6c6fa548f50d07d7fce452749aabec37248c33
kleer <kleer@kleer-csd-box>
2018-05-11 14:11:56 -0500

    prueba ok

--- lib/blackicolpa.rb
+++ lib/blackicolpa.rb
@@ -1,2 +1,22 @@
 class Blackicolpa
+	def carta_aleatoria
+	palos = %w{ corazones treboles picas diamantes}
+  	numero = %w{ 1 2 3 4 5 6 7 8 9 10 J Q K }
+    
+    #palo aleatorio
+  	num = palos.length
+  	palo_aleat = rand(num)
+ 
+  	#numero aleatorio
+  	num_aleat = rand(numero.length)
+ 
+  	puts numero[num_aleat] + ' de ' + palos[palo_aleat] 
+	end
+
+	#def asignar_carta
+	#	carta1 = numero[num_aleat] + ' de ' + palos[palo_aleat]
+	#	@carta2 = numero[num_aleat] + ' de ' + palos[palo_aleat]
+	#puts carta1  
+	#'y su carta 2 es ' @carta2
+	#end
 end
--- spec/blackicolpa_spec.rb
+++ spec/blackicolpa_spec.rb
@@ -1,8 +1,8 @@
-#require './lib/blackicolpa.rb'
+require './lib/blackicolpa.rb'
 
-#describe Carta do 
-#	it "entregar una carta" do		
-#	pf = Carta.new
-#	expect(pf.nueva).to eq "2P" 
-#	end
-#end
+describe Blackicolpa do 
+	it "entregar una carta" do		
+	pf = Blackicolpa.new
+	expect(pf.carta_aleatoria) 
+	end
+end
