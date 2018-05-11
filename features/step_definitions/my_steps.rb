e185ed00e712140342099ea1a53f0eaa61d9e5ee
kleer <kleer@kleer-csd-box>
2018-05-11 13:21:13 -0500

    er

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -9,3 +9,18 @@ end
 Then(/^debo ver carta dos como "([^"]*)"$/) do |texto|
   find("h3").text match /#{texto}/m
 end
+
+Given(/^ya vi mis dos cartas$/) do |texto|
+  step 'que abro el juego'
+  step 'debo ver carta uno como "2P"'
+  step 'debo ver carta dos como "2T"'
+end
+
+Then(/^debo ver carta uno del oponente como "([^"]*)"$/) do |texto|
+  step 'debo ver "carta uno del oponente como "9P"'
+  step 'debo ver "carta dos del oponente como "5C""'
+end
+
+Then(/^debo ver carta dos del oponente como "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
